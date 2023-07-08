cef235cfc07e1f5dcfdf099fc69bb27c7e499fb5
Minaya Yagubova <minayaqubova@gmail.com>
2023-07-08 23:59:28 +0400

    updated

--- millioner/main.swift
+++ millioner/main.swift
@@ -6,100 +6,82 @@
 //
 
 import Foundation
-print("Add your name")
-var name = readLine()
-
-var questions = ["1+1",
-                 "2+1",
-                 "3+1",
-                 "4+1",
-                 "5+1",
-                 "6+1",
-                 "7+1",
-                 "8+1",
-                 "9+1",
-                 "10+1",
-                 "11+1",
-                 "12+1",
-                 "12+1",
-                 "14+1",
-                 "15+1",
+let questions = ["Azebaycanin paytaxti",
+                 "Son dunya cempionu",
+                 "Amerikada nece stat var",
+                 "Hansi dovlet AB'de yoxdu",
+                 "Azebaycanin paytaxti",
+                 "Son dunya cempionu",
+                 "Amerikada nece stat var",
+                 "Hansi dovlet AB'de yoxdu",
+                 "Azebaycanin paytaxti",
+                 "Son dunya cempionu",
+                 "Amerikada nece stat var",
+                 "Hansi dovlet AB'de yoxdu",
+                 "Azebaycanin paytaxti",
+                 "Son dunya cempionu",
+                 "Amerikada nece stat var"
 ]
 
-var answers = ["0": ["A) 1", "B) 3", "C) 4", "D)2"],
-               "1": ["A) 3", "B) 8", "C) 5", "D)1"],
-               "2": ["A) 4", "B) 3", "C) 8", "D)7"],
-               "3": ["A) 4", "B) 5", "C) 8", "D)7"],
-               "4": ["A) 6", "B) 3", "C) 8", "D)7"],
-               "5": ["A) 4", "B) 3", "C) 8", "D)7"],
-               "6": ["A) 4", "B) 3", "C) 8", "D)7"],
-               "7": ["A) 4", "B) 3", "C) 9", "D)7"],
-               "8": ["A) 10", "B) 3", "C) 8", "D)7"],
-               "9": ["A) 11", "B) 3", "C) 8", "D)7"],
-               "10": ["A) 12", "B) 3", "C) 8", "D)7"],
-               "11": ["A) 4", "B) 13", "C) 8", "D)7"],
-               "12": ["A) 4", "B) 3", "C) 14", "D)7"],
-               "13": ["A) 4", "B) 15", "C) 8", "D)7"],
-               "14": ["A) 4", "B) 3", "C) 8", "D)16"],
+let answers = ["0": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "1": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "2": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "3": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "4": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "5": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "6": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "7": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "8": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "9": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "10": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "11": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "12": ["A) 11", "B) 22", "C) 33", "D) 44"],
+               "13": ["A) asd", "B) qwe", "C) zxc", "D) rty"],
+               "14": ["A) 11", "B) 22", "C) 33", "D) 44"]]
 
-]
+let correctAnswer = ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C", "A", "B", "C"]
 
-var trueAnswers = [
-    "D",
-    "A",
-    "A",
-    "B", "A", "D","C","C","A","A","A","B","C","B","D"
-]
+print("Enter your name")
+let name = readLine()
 
-var points = 0
-
-
+var point = 0
 for (index, question) in questions.enumerated() {
     print(question)
-    print(answers["\(index)"] ?? "") //answer["0"]
-    let userAnswer = readLine()
-    if userAnswer == "0" {
-        print("Game Over")
+    print(answers["\(index)"] ?? "")
+    let myAnswer = readLine()?.uppercased()
+    
+    if index >= 10 && myAnswer == "0" {
+        print("Siz \(point) ile oyunu tamamladiniz")
+        break
     }
-    for _ in 1...5{
-        if userAnswer == trueAnswers[index] {
-            points += 1
-            print("Your points: \(points)")
-        } else {
-            points = 0
-            print("Game over. Your points: \(points)")
+    
+    if myAnswer == correctAnswer[index] {
+        print("Dogru cavab")
+        if index >= 0 && index < 5 {
+            point += 1
+        } else if index >= 5 && index < 10 {
+            point += 3
+        } else if index >= 10 {
+            point += 5
+        }
+        
+        print("Sizin \(point) xaliniz var")
+    } else {
+        print("Sehv cavab")
+        if index >= 0 && index < 5 {
+            point = 0
+            print("Siz uduzdunuz. Xal: \(point)")
             break
-            
-        }
-    }
-    for _ in 6...10{
-        if userAnswer == trueAnswers[index] {
-            points += 3
-            print("Your points: \(points)")
-        } else {
-            print("Game over. Your points: 5")
+        } else if index >= 5 && index < 10 {
+            point = 5
+            print("Siz uduzdunuz. Xal: \(point)")
             break
-            
+        } else if index >= 10 {
+            point = 20
+            print("Siz uduzdunuz. Xal: \(point)")
         }
-   //     var exitPoints = points
+        break
     }
-    for _ in 11...15{
-        if userAnswer == trueAnswers[index] {
-            points += 5
-            print("Your points: \(points)")
-        } else {
-            points = 0
-            print("Game over. Your points: \(points)")
-            break
-            
-        }
-        let userAnswer = readLine()
-        if userAnswer == "0" {
-            print("Game Over.Your points: \(points) ") //exitPoints??
-        }
-    }
-    
-    
-    
-    
 }
+
+print("Siz qalibsiniz. Xal: \(point)")
